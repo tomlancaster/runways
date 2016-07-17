bede1a175752022f5535b35cb0562a991ffef32a
Tom Lancaster <trlancaster@gmail.com>
2016-07-17 17:13:01 -0400

    adds top nav and index page; adds report route and base html file

---
+++ app/controllers/ReportController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+
+import services.CountryQuerier
+
+/**
+  * Created by tom on 7/17/16.
+  */
+class ReportController @Inject() (countryQuerier: CountryQuerier) extends Controller {
+
+  def index = Action {
+    Ok(views.html.report("Behold! Your Report!"))
+  }
+
+}
